a2bdf1461f47d6473f456007e4357c3a8a7dda83
korabel <igorlonkai@gmail.com>
2022-04-10 21:44:47 +0300

    double menu added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ class App extends Component {
             <div>
                 <Home />
                 <About />
+                <Menu />
+                <Menu />
                 <Contact />
             </div>
         )
