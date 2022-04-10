77c6a705da263fb5513b232e0adcea3dda1ba842
korabel <igorlonkai@gmail.com>
2022-04-10 21:44:17 +0300

    menu removed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class App extends Component {
             <div>
                 <Home />
                 <About />
-                <Menu />
                 <Contact />
             </div>
         )
